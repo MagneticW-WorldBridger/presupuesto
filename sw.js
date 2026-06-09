9053a33aaaa3f82be9bad77d4b9f06122aff1ba5
Jean de Lasse <jean.ps3.ufo@gmail.com>
2026-06-09 10:49:05 -0500

    category tap shows its movements with remove (X); add inline; global history separate

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='dinero-v3';
+const CACHE='dinero-v4';
 const ASSETS=['./','./index.html','./manifest.json','./icon-192.png','./icon-512.png','./apple-touch-icon.png'];
 self.addEventListener('install',e=>{self.skipWaiting();e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).catch(()=>{}))});
 self.addEventListener('activate',e=>{e.waitUntil(caches.keys().then(ks=>Promise.all(ks.map(k=>k!==CACHE&&caches.delete(k)))).then(()=>self.clients.claim()))});
